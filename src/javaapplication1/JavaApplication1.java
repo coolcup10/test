de23e4b75d93495cc6a967cb0971df79fc8851b4
Vincent C. Strzelecki <vsou812@aol.com>
2018-03-12 11:29:02 -0400

    new commit2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -13,6 +13,8 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
    System.out.print("hello");
+  System.out.print("hello");
+  System.out.print("hello");
     }
     
 }
